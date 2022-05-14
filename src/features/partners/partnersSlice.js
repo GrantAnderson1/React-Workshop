640120111118c9562677176846628305c6f45a0d
GrantAnderson1 <102125461+GrantAnderson1@users.noreply.github.com>
2022-05-14 13:27:12 -0500

    Workshop 2nd Week

---
+++ src/features/partners/partnersSlice.js
@@ -0,0 +1,12 @@
+import { PARTNERS } from '../../app/shared/PARTNERS';
+
+
+
+export const selectAllPartners = () => {
+    return PARTNERS;
+};
+
+
+export const selectFeaturedPartners = () => {
+    return PARTNERS.find((partner) => partner.featured)
+};
